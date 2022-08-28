13a0eaa6f46fead2df63bb23e5f0353e12217cda
Pankaj Poshirkar <poshirkar@gmail.com>
2022-08-28 21:20:30 +0200

    FeatureClass1.java

---
+++ src/FeatureClass1.java
@@ -0,0 +1,6 @@
+public class FeatureClass1 {
+    public static void main(String[] args) {
+        System.out.println("This is feature branch class");
+        System.out.println("This is FeatureClass1 class");
+    }
+}
