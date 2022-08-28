0fc23de334c71bd88deb0259997589ae2933411a
Pankaj Poshirkar <poshirkar@gmail.com>
2022-08-28 21:24:15 +0200

    Added MainDemo1 and MainDemo2 classes

---
+++ src/MainDemo1.java
@@ -0,0 +1,7 @@
+public class MainDemo1 {
+    public static void main(String[] args) {
+
+        System.out.println("This is main branch class");
+        System.out.println("This is MainDemo1 class");
+    }
+}
---
+++ src/MainDemo2.java
@@ -0,0 +1,7 @@
+public class MainDemo2 {
+    public static void main(String[] args) {
+
+        System.out.println("This is main branch class");
+        System.out.println("This is MainDemo2 class");
+    }
+}
